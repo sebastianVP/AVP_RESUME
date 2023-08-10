41d73f5548d5417767726dfc296834c8222ef74c
Alexander Valdez <alex.valdezp22@gmail.com>
2023-08-10 01:15:12 -0500

    cv app

--- cv_avp_app.py
+++ cv_avp_app.py
@@ -72,8 +72,8 @@ st.markdown('''
 
 txt('**Bachelors of Science** (Electronic Engineer), *Univ. Nacional de Ingenieria* , Lima-Peru','2006-2010')
 st.markdown('''
--  Graduated
--  Thesis: `Monitoring System and Early Warning of Alluvium in Huaycoloro's ravine for the Geophysical Institute of Peru and SEDAPAL` [ Link ](https://repositorio.igp.gob.pe/bitstream/handle/20.500.12816/4449/Valdez%2c%20A.%202015..pdf?sequence=1&isAllowed=y)
+-  Graduated.
+-  Thesis: `Monitoring System and Early Warning of Alluvium in Huaycoloro's ravine for the Geophysical Institute of Peru and SEDAPAL.` [Thesis](https://repositorio.igp.gob.pe/bitstream/handle/20.500.12816/4449/Valdez%2c%20A.%202015..pdf?sequence=1&isAllowed=y)
 -  XXIII CONEIMERA CONGRESS `1st` Place Call for Papers Universidad Nacional de Piura Competition.
 ''')
 
@@ -85,14 +85,75 @@ st.markdown('''
 txt('**Radar Specialist**, Instituto Geofisico del Peru' , '2020- Present')
 
 st.markdown('''
-- Project Weather Radar: Equipment design and integration to operate the `1st` Meteorological Radar developed in Peru
+- Radar SOPHy: Scaning-system for Observations of Peruvian Hydrometerological-event, Monitoring and maintenance.
+[SOPHy](https://doi.org/10.1109/INCAS53599.2021.9666928)
+- Project Weather Radar: Equipment design and integration to operate the `1st` Meteorological Radar developed in Peru - SOPHy.
 - Project Signal Chain: Develop libraries  for radar data processing.
--
 ''')
 
 
+txt('**Development Engineer**, Instituto Geofisico del Peru','2013-2020')
 
+st.markdown('''
+- Project HF Systems: Reading Module development information, process data and plots results. Monitor operation of  transmission and reception stations in different locations within Peru.
+- Project TIDDBIT Systems: Monitor status and perform maintenance of transmission stations.
+- Project Automatic Beam  Switching: Software test development and hardware with high voltage test and control modules with RF Modules.
+- Porject Monitoring Systems and Early Warming of Alluvium: Development of a prototype, design and development of the software acquisition and monitoring, including the development of a Web Interface and field testing.  
+- Operation, maintenance and  supervision of the Early Alert System installed in the JRO.
+''')
 
+txt('**Jicamarca Engineering Program-Internship**, MIT Haystack Observatory','Jun 2019-Jul 2019')
 
+st.markdown('''
+- Project Measuring Meteor winds  using Radio Array of Portable Interferometric Detectors(RAPID).[Link](https://m.facebook.com/MITHaystack/photos/a.1102522826429093/2741805839167442/)
+''')
 
+txt('**Research Assistant**, International Science','Jan 2012- Dec 2012')
 
+st.markdown('''
+- Development of a Graphical User Interface using Python, QT4Designer, IDE Eric4 and Eclipse.
+- Module Development: Reading, processing and visualization using Python. Calculate the Spectra at heights and save data in FITS format.
+''')
+
+txt('**Research Assitant**, Universidad Tecnologica del Peru','Oct 2011- Aug 2012')
+st.markdown('''
+- Development and application of PID and status controllers in laboratories.
+- Linux Driver Development.
+- Linux for embedded and Real-time Applications.
+''')
+
+txt('**Biomedical Research Advisor**, Equipmed Americas','Jan 2011. Dec 2011')
+st.markdown('''
+- Technical assistance, repair and installation of medical equipment.
+''')
+
+######################
+
+st.markdown('''
+## Radar Specialist Tools
+''')
+
+txt4('GNURadio','Free & open-source software development toolkit that provides signal processing blocks to implement software radios','https://www.gnuradio.org/')
+txt4('SignalChain',' Python-based radar signal processing library developed at the Jicamarca Radio Observatory','http://jro.igp.gob.pe:8082/schain')
+
+#######################
+
+st.markdown('''
+## Skills
+''')
+
+txt3('Programming','`Python`,`Linux`')
+txt3('Data processing','`numpy`,`pandas`,`SQL`')
+txt3('Data visualization','`matplotlib`,`seaborn`,`plotly`')
+txt3('Machine Learning','`scikit-learn`')
+txt3('Deep Learning','`TensorFlow`')
+txt3('Web development','`Flask`,`HTML`,`CSS`')
+txt3('Model deployment','`streamlit`,`Heroku`,`pythonanywhere`')
+
+######################
+st.markdown('''
+## Social Media
+''')
+
+txt2('LinkedIn','')
+txt2('GitHub','')
