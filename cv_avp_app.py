d1801c66138422728a046861c118301d96c1533a
Alexander Valdez <alex.valdezp22@gmail.com>
2023-08-10 02:13:17 -0500

    barra con indice

--- cv_avp_app.py
+++ cv_avp_app.py
@@ -25,7 +25,38 @@ st.info('''
 research apply within government institutions.
 - Project leader with the ability to build productive cross-functional teamas.
 ''')
+#####################
+# Navigation
 
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
+  <a class="navbar-brand" href="https://github.com/sebastianVP" target="_blank">Alexander Valdez</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#education">Education</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#work-experience">Work Experience</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#radarspecialist-tools">RadarSpecialist Tools</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#social-media">Social Media</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+""", unsafe_allow_html=True)
 
 #####################
 # Custom function for printing text
@@ -131,7 +162,7 @@ st.markdown('''
 ######################
 
 st.markdown('''
-## Radar Specialist Tools
+## RadarSpecialist Tools
 ''')
 
 txt4('GNURadio','Free & open-source software development toolkit that provides signal processing blocks to implement software radios','https://www.gnuradio.org/')
